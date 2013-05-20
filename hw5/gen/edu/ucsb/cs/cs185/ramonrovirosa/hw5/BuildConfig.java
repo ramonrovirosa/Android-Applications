ded58befc30126a69382a6ce6d67deed6d59b2aa
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2013-05-20 13:40:53 -0700

    rm hw5

--- hw5/gen/edu/ucsb/cs/cs185/ramonrovirosa/hw5/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.ucsb.cs.cs185.ramonrovirosa.hw5;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
